53da46f272cf5bb946aa6938e89fda9027734b99
Halim <61380938+book1001@users.noreply.github.com>
2022-05-09 07:57:57 -0400

    Add files via upload

---
+++ 2_UI_drawer.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("costumeSidenav").style.width = "150px";
+}
+
+function closeNav() {
+  document.getElementById("costumeSidenav").style.width = "0";
+}
